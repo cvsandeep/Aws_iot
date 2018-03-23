81f89f879904ae2deac31f7cba09a9670e1305a5
sandeep <cvs.sandeep@gmail.com>
2018-03-23 11:59:42 -0700

    Updated init

--- device1.py
+++ device1.py
@@ -1,25 +1,59 @@
-import paho.mqtt.client as mqtt
-import paho.mqtt.publish as publish
-import logging, logging.handlers, traceback
-import json, math, time, sys, ssl
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import logging
+import time
+import argparse
+import json
 
 cert_path = "/home/pi/certs/"
 host = "a2kc9la4cp40qj.iot.us-east-1.amazonaws.com"
-returntopic = "$aws/things/cvshome/shadow/return"
-sendtopic = "$aws/things/cvshome/shadow/send"
+returntopic = "$aws/things/ir2_proj/shadow/return"
+sendtopic = "$aws/things/ir2_proj/shadow/send"
 root_cert = "/home/pi/certs/root-CA.crt"
-cert_file = cert_path + "77c7086401-certificate.pem.crt"
-key_file = cert_path + "77c7086401-private.pem.key"
+cert_file = cert_path + "ir2_proj.cert.pem"
+key_file = cert_path + "ir2_proj.private.key"
 
 
-robot= AWSIoTMQTTClient(clientId)
+# Intializes and sets Logging for the service
+def logger_init():
+    logger.setLevel(logging.DEBUG)
+    log_file_size = 1024 * 1024 * 1  # 1 MB
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(process)d - %(name)s : %(message)s')
+    fh = logging.handlers.RotatingFileHandler('/home/pi/logs/iot_robot.log', maxBytes=log_file_size, backupCount=5)
+    fh.setFormatter(formatter)
+    sh = logging.StreamHandler(sys.stdout)
+    sh.setFormatter(formatter)
+    logger.addHandler(fh)
+    logger.addHandler(sh)
+    logger.info('******************************************')
+    logger.info('.......Starting up proj Service........')
+    
 
-oyAWSIoTMQTTClient.configureEndpoint(host, 8883)
-myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+# Initializing Logger      
+logger_init()
+logger.info('setting up mqtt client')
+
+#starting service
+robot = AWSIoTMQTTClient(clientId)
+robot.configureEndpoint(host, 8883)
+robot.configureCredentials(root_cert, key_file, cert_file)
+logger.info('completed setting up mqtt client')
 
 # AWSIoTMQTTClient connection configuration
-  myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
-            myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-            myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
-            myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
-            myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+robot.configureAutoReconnectBackoffTime(1, 32, 20)
+robot.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+robot.configureDrainingFrequency(2)  # Draining: 2 Hz
+robot.configureConnectDisconnectTimeout(10)  # 10 sec
+robot.configureMQTTOperationTimeout(5)  # 5 sec
+logger.info('completed setting up mqtt client')
+
+# Connect and subscribe to AWS IoT
+myAWSIoTMQTTClient.connect()
+
+robot.loop()
+time.sleep(1)
+robot.publish(sendtopic, 'MyHomeService started')
+
+#loops for ever so that connection will be present
+while True:
+	robot.loop()
+	time.sleep(1)
